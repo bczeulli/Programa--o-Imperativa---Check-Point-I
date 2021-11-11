fd8294aa40dc40041b3c59b1f78af1d828371a36
Bruno Cesar Zeulli <78604226+bczeulli@users.noreply.github.com>
2021-11-10 22:09:27 -0300

    Update microondas.js

--- microondas.js
+++ microondas.js
@@ -7,60 +7,56 @@
 // 5 - Brigadeiro – 8 segundos (tempo padrão)
 
 
-function prepararComida (prato,segundos){
+function microondas (prato,segundos){
     let comidaPronta = "Prato inexistente"
     
-    if (prato == 1 && segundos >= 10 && segundos <= 19){
+    if (prato == 1 && segundos >= 10 && segundos <= 20){
         comidaPronta = "Pipoca pronta, bom apetite!!!"
-    } else if (prato == 1 && segundos >= 20 && segundos <= 29){
-        comidaPronta = "A pipoca queimou!"
-    } else if (prato == 1 && segundos >= 30){
-        comidaPronta = "KABUMMM!"
+    } else if (prato == 1 && segundos >= 21 && segundos <= 30){
+        comidaPronta = "A pipoca queimou! Bom apetite!"
+    } else if (prato == 1 && segundos >= 31){
+        comidaPronta = "KABUMMM! Bom apetite!"
     } else if  (prato == 1 && segundos <= 9){
         comidaPronta = "Tempo insuficiente."
     }
 
-    if (prato == 2 && segundos >= 8 && segundos <= 15){
+    if (prato == 2 && segundos >= 8 && segundos <= 16){
         comidaPronta = "Macarrão pronto, bom apetite!!!"
-    } else if (prato == 2 && segundos >= 16 && segundos <= 23){
-        comidaPronta = "O macarrão queimou!"
-    } else if (prato == 2 && segundos >= 24){
-        comidaPronta = "KABUMMM!"
+    } else if (prato == 2 && segundos >= 17 && segundos <= 24){
+        comidaPronta = "O macarrão queimou! Bom apetite!"
+    } else if (prato == 2 && segundos >= 25){
+        comidaPronta = "KABUMMM! Bom apetite!"
     } else if  (prato == 2 && segundos <= 7){
         comidaPronta = "Tempo insuficiente."
     }
-
-    if (prato == 3 && segundos >= 15 && segundos <= 29){
+    if (prato == 3 && segundos >= 15 && segundos <= 30){
         comidaPronta = "Carne pronta, bom apetite!!!"
-    } else if (prato == 3 && segundos >= 30 && segundos <= 44){
-        comidaPronta = "A carne queimou!"
-    } else if (prato == 3 && segundos >= 45){
-        comidaPronta = "KABUMMM!"
+    } else if (prato == 3 && segundos >= 31 && segundos <= 45){
+        comidaPronta = "A carne queimou! Bom apetite!"
+    } else if (prato == 3 && segundos >= 46){
+        comidaPronta = "KABUMMM! Bom apetite!"
     } else if  (prato == 3 && segundos <= 14){
         comidaPronta = "Tempo insuficiente."
     }
-
-    if (prato == 4 && segundos >= 12 && segundos <= 23){
+    if (prato == 4 && segundos >= 12 && segundos <= 24){
         comidaPronta = "Feijão pronto, bom apetite!!!"
-    } else if (prato == 4 && segundos >= 24 && segundos <= 35){
-        comidaPronta = "O feijão queimou!"
-    } else if (prato == 4 && segundos >= 36){
-        comidaPronta = "KABUMMM!"
+    } else if (prato == 4 && segundos >= 25 && segundos <= 36){
+        comidaPronta = "O feijão queimou! Bom apetite!"
+    } else if (prato == 4 && segundos >= 37){
+        comidaPronta = "KABUMMM! Bom apetite!"
     } else if  (prato == 4 && segundos <= 11){
         comidaPronta = "Tempo insuficiente."
     }
-
     if (prato == 5 && segundos >= 8 && segundos <= 15){
         comidaPronta = "Brigadeiro pronto, bom apetite!!!"
     } else if (prato == 5 && segundos >= 16 && segundos <= 23){
-        comidaPronta = "O brigadeiro queimou!"
+        comidaPronta = "O brigadeiro queimou! Bom apetite!"
     } else if (prato == 5 && segundos >= 24){
-        comidaPronta = "KABUMMM!"
+        comidaPronta = "KABUMMM! Bom apetite!"
     } else if  (prato == 5 && segundos <= 7){
         comidaPronta = "Tempo insuficiente."
     }
-    
-    return comidaPronta
+    return console.log(comidaPronta)
     }
-    console.log(prepararComida(2,20))
+    microondas(1,16)
     
